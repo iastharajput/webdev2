8ba72827d0602d577383bf7816fe7e07387251fe
iastharajput <iastharajput@gmail.com>
2025-05-26 21:18:49 +0530

    functions

---
+++ javascript/video58/index.js
@@ -0,0 +1,23 @@
+function nice(name) {
+
+    console.log("hey " + name + ", you are nice");
+    console.log("hey " + name + ", you are good");
+    console.log("hey " + name + ", your course is good");
+}
+
+// console.log("hey harry you are nice");
+// console.log("hey harry you are good");
+// console.log("hey harry your course is good");
+nice("rohan");
+nice("shivam");
+
+function add(a, b) {
+    console.log("sum is " + (a + b));
+}
+add(5, 6);
+add(10, 20);
+function sum(a, b) {
+    return a + b;
+}
+let ans = sum(5, 6);
+console.log(ans);
