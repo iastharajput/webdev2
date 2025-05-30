1db3c5f1a3afd471a56461ceafa0e32c866bab51
iastharajput <iastharajput@gmail.com>
2025-05-30 21:38:39 +0530

    datatypes

---
+++ javascript/datatypes/index.js
@@ -0,0 +1,15 @@
+"use strict" //treat all JS code as newer version//
+
+// alert( 3 + 3 ); // we are using strict mode, so this will not throw an error  
+console.log("Aastha")
+
+let name = "Aastha"; // string
+let age = 217  ; // number
+let isStudent = true; // boolean
+let hobbies = ["reading", "coding", "hiking"]; // array
+let state = null; // null
+let address; // undefined
+
+console.log(typeof age)
+console.log(typeof state) 
+console.log(typeof address)
