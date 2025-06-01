993598febff0cda5ec5cbbaafc09f3003d700702
iastharajput <iastharajput@gmail.com>
2025-06-01 19:57:50 +0530

    conversion

---
+++ javascript/datatype_conversion/index.js
@@ -0,0 +1,18 @@
+let score = 33
+
+// console.log(typeof score) // number
+
+// let valueinNumber = Number(score)
+// console.log(typeof valueinNumber) // number
+// console.log(valueinNumber) 
+// "33" is converted to a number, so it remains 33
+// "33abc"
+let isLoggedin = 1
+
+// let booleanIsLoggedin = Boolean(isLoggedin)
+// console.log(booleanIsLoggedin) // true
+
+let anumber=33
+let stringNumber = String(anumber)
+console.log(stringNumber) 
+console.log(typeof stringNumber)
