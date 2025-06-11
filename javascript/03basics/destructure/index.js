d3bb8cb44077839f04cf8ae33e9aebb3b4df6ada
iastharajput <iastharajput@gmail.com>
2025-06-11 23:02:25 +0530

    object de-structure

---
+++ javascript/03basics/destructure/index.js
@@ -0,0 +1,26 @@
+const course ={
+coursename: "js in hindi ",
+price: "199",
+courseid: "js123",
+courseInstructor: "sahil"
+};
+// course.courseInstructor
+
+// const { coursename, price, courseid, courseInstructor } = course;
+// console.log(coursename, price, courseid, courseInstructor);
+
+const {courseInstructor: instructor } =course //here we are renaming the variable
+
+console.log(instructor); 
+
+{
+    name : "sahil",
+    coursename: "js in hindi",
+    price: "199",
+}
+
+[
+    {}''
+    {},
+    {}
+]
